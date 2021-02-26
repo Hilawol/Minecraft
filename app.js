409fe0af3dd15da37586eac77b32ae01a4ce9230
Hila Kershenovich <hilawol@gmail.com>
2021-02-27 00:46:34 +0200

    added remove/add cells to game

--- app.js
+++ app.js
@@ -4,36 +4,82 @@ const dirtTop = dirtStart + 1;
 
 let selectedTool;
 let selectedTile;
+
+/**
+ *  Represent the state of the game:
+ *   Remove = 0 . When a tool is selected- we can only remove tiles
+ *   Add = 1. When a tile type is selected- we can only add this kind of tile.
+ * */
+let gameState = {
+  remove: 0,
+  add: 1,
+};
+let currentState;
 const tools = []; //Holds all type of tools names and id's.
 const tiles = [];
+// const counters = [];
 
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", () => window.location.reload());
 
-const setAllowedTiles = () => {
+const setAllowedTiles = () => {};
 
+/**
+ *  Removes the class: className from element. 
+ * If element is not provided, search for the document for an element. 
+ */
+const removeClass = (className, element = null) => {
+  if (element) {
+    if(element.classList.contains(className)){
+      element.classList.remove(className);
+    }
+  }
+  else{
+    let s = document.querySelector(`.${className}`);
+    if (s) {
+      s.classList.remove(className);
+    }
+  }
+};
+
+const setSelectedElement = (type, element) => {
+  let name = element.getAttribute("name");
+  if (type === "tool") {
+    selectedTool = tools.find((tool) => tool.name === name);
+  } else if (type == "tile") {
+    selectedTile = tiles.find((tile) => tile.name === name);
+  }
+  element.classList.add("selected");
+  console.log("selected:" + name);
+  console.log("current state:" + currentState);
 };
 
 const clickOnTool = (e) => {
-  if (selectedTool) { //removes the selected class from the previous selected tool
-    document.querySelector(`.${selectedTool.name}`).classList.remove("selectedTool");
-  }
-  selectedTool = tools.find(tool => //sets the new selected tool 
-    tool.name === e.currentTarget.getAttribute("name"));
-  e.currentTarget.classList.add("selectedTool");
-  setAllowedTiles();//TODO setAllowedTiles
+  currentState=gameState.remove;
+  removeClass("selected");
+  setSelectedElement("tool", e.currentTarget);
+  setAllowedTiles(); //TODO setAllowedTiles
 };
 
-const clickOnTile = (e)=>{
-  debugger
-  if (selectedTile) { //removes the selected class from the previous selected tool
-    document.querySelector(`.${selectedTile.name}`).classList.remove("selectedTile");
-  }
-  selectedTile = tiles.find(tile => //sets the new selected tile
-    tile.name === e.currentTarget.getAttribute("name"));
-    e.currentTarget.classList.add("selectedTile");
-}
+const clickOnTile = (e) => {
+  currentState=gameState.add;
+  removeClass("selected");
+  setSelectedElement("tile", e.currentTarget);
+};
 
+/**
+ * 
+ * The event listener callback faction for each cell in matrix
+ */
+function doOnClick(e) {
+  console.log(e.currentTarget.dataset);
+  if (currentState === gameState.remove) {
+    removeTile(e.currentTarget);
+  }
+  else if (currentState === gameState.add){
+    addTile(e.currentTarget);
+  }
+}
 
 const toolTypes = document.querySelectorAll(".tool");
 const createTools = (toolTypes) => {
@@ -41,53 +87,100 @@ const createTools = (toolTypes) => {
     let toolObj = {
       name: tool.getAttribute("name"),
       id: index,
-    }
+    };
     tools.push(toolObj);
     tool.addEventListener("click", clickOnTool);
   });
-}
+};
 createTools(toolTypes);
 
+/**
+ * Define for tools[] which tile it can remove
+ */
+const setAllowedToRemove = () => {
+  tools.forEach(tool =>{
+    switch (tool.name){
+      case "axe":
+        tool.allow=["treeTop","treeLog"];
+        break;
+      case "pickaxe":
+        tool.allow=["rock"];
+        break;
+        break;
+      case "shovel":
+        tool.allow=["dirt","dirtTop"];
+        break;
+    }
+  })
+};
+setAllowedToRemove();
+console.log(tools);
 
-const tileTypes = document.querySelectorAll(".inventoryBox");
+const tileTypes = document.querySelectorAll(".tile");
 const createTiles = (tileTypes) => {
   tileTypes.forEach((tile, index) => {
     let tileObj = {
       name: tile.getAttribute("name"),
       id: index,
-      count: tile.firstElementChild.innerText,
-    }
+      count: Number(tile.firstElementChild.innerText),
+    };
     tiles.push(tileObj);
-    tile.addEventListener("click",clickOnTile);
-  })
-}
+    tile.addEventListener("click", clickOnTile);
+  });
+};
 createTiles(tileTypes);
+console.log(tiles);
 
+const updateCounter = (type) => {
+  currentState === gameState.remove? tiles[type.id].count += 1:
+  tiles[type.id].count -= 1;
+  document.querySelector(`.${type.name} .counter`).textContent =
+    tiles[type.id].count;
+};
 
-function doOnClick(e) {
+const removeTile = (tile) => {
+  let type = getTileType(tile);
+  if (type) {
+    //if type is undefined the tile is "sky" and can't br removed
+    removeClass(type.name, tile);
+    updateCounter(type);
+  }
+};
 
-  console.log(e.currentTarget.dataset);
-
+const addTile = (tile)=>{
+  let type = getTileType(tile); //The type of the tile to add to. Can only add  tiles on "sky"
+  if(!type){
+    if (tiles[selectedTile.id].count>0){
+      tile.classList.add(selectedTile.name);
+      updateCounter(selectedTile);
+    }
+    console.log("tile is sky",tile);
+  }
 }
 
+const getTileType = (selectedTile) =>
+  tiles.find((tile) => selectedTile.classList.contains(tile.name));
+
+const getCell = (col,row)=>{
+  let elements = document.querySelector(".pixel");
+
+} 
 const buildWorld = () => {
   for (let row = 0; row < 25; row++) {
     for (let col = 0; col < 35; col++) {
       let pixel = document.createElement("div");
       pixel.setAttribute("data-col", col);
-      pixel.setAttribute("data-row", row)
+      pixel.setAttribute("data-row", row);
       pixel.classList.add("pixel");
       pixel.addEventListener("click", doOnClick);
       if (row == dirtTop) {
         pixel.classList.add("dirtTop");
-      }
-      if (row > dirtStart) {//dirt
+      } else if (row > dirtStart) {
         pixel.classList.add("dirt");
       }
-      pixel.classList.add("sky");
       matrixDiv.appendChild(pixel);
     }
   }
-}
-
+};
 buildWorld();
+
